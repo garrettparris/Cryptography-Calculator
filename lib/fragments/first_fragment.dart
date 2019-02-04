11ac5e281054b8a7cb6f55f4471f0a74b8f1c456
Garrett <garrettp18@gmail.com>
2019-02-03 19:04:11 -0500

    added template for modulo

--- lib/fragments/first_fragment.dart
+++ lib/fragments/first_fragment.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'dart:core';
 
 class FirstFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Center(
-      child: new Text("Home Page"),
+    return new Scaffold(
+      body: new Container(
+          padding: const EdgeInsets.all(40.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new TextField(
+                decoration: new InputDecoration(labelText: "Enter your number"),
+                keyboardType: TextInputType.number,
+              ),
+            ],
+          )),
     );
   }
 
+  modulo(int exponent, int dividend, int divisor) {
+    if (exponent == 1) {
+      return dividend % divisor;
+    } else if (exponent == -1) {
+      return dividend.modInverse(divisor);
+    } else if (!exponent.isNegative && exponent != 1) {
+      return dividend.modPow(exponent, divisor);
+    } else {
+      //broke
+    }
+  }
 }
--- lib/fragments/second_fragment.dart
+++ lib/fragments/second_fragment.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:core';
 
 class SecondFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Center(
-      child: new Text("Hello Fragment 2"),
+    return new Scaffold(
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Container(
+                  width:50,
+                  child: new TextField(
+                    textAlign: TextAlign.center,
+                    decoration: new InputDecoration(
+                        hintText: "y", contentPadding: new EdgeInsets.all(0.0)),
+                  ),
+                ),
+                new Container(
+                  width: 50,
+                  child: new TextField(
+                    textAlign: TextAlign.center,
+                    decoration: new InputDecoration(
+                        hintText: "y", contentPadding: new EdgeInsets.all(0.0)),
+                  ),
+                ),
+                new Container(
+                  width: 50,
+                  child: new TextField(
+                    textAlign: TextAlign.center,
+                    decoration: new InputDecoration(
+                        hintText: "y", contentPadding: new EdgeInsets.all(0.0)),
+                  ),
+                ),
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                new Container(),
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                new Container(),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
+  modulo(int exponent, int dividend, int divisor) {
+    if (exponent == 1) {
+      return dividend % divisor;
+    } else if (exponent == -1) {
+      return dividend.modInverse(divisor);
+    } else if (!exponent.isNegative && exponent != 1) {
+      return dividend.modPow(exponent, divisor);
+    } else {
+      //broke
+    }
+  }
 }
